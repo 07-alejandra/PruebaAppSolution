981686ac1635333800448e610ff9ce6a22b58fc2
07-alejandra <a17050002@unisierra.edu.mx>
2020-09-18 18:21:30 -0700

    Modelo PErson conMVC Emtity Framework

---
+++ PruebaAppGit/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaAppGit.Models
+{
+    public class Person
+    {
+        //Propierties
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
